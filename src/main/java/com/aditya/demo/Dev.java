e6f30e5956fb667a6a4fd1349d4ad2ee0edb5e36
adityaaryan <adityaaryan531@gmail.com>
2026-04-27 02:51:56 +0530

    Simple example

--- src/main/java/com/aditya/demo/Dev.java
+++ src/main/java/com/aditya/demo/Dev.java
@@ -5,9 +5,21 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
-    @Autowired
+    @Autowired  // field injection (field injection is not preffered)
     private Work work; // to run checkLogs function we need object of Work class and we cant create a new object
     //  we need to map the existing created object inside ioc container to this which is done using Autowired Annotation
+
+    // can also be done by constructor injection (autowiring is optional for constructor injection but mandatory for field & setter)
+    //    public Dev(Work work){
+    //        this.work=work;
+    //    }
+
+    // can also be done by setter injection
+//    @Autowired
+//    public void setLaptop(Work work){
+//        this.work=work;
+//    }
+
     public void build(){
         work.checkLogs();
         System.out.println("Building stuff");
